30fab05938662afee8c3fb8bcb1a7611d4004f10
clc babischkin <n/a>
2021-01-19 16:46:40 -0600

    Initial Commit

---
+++ BussinessApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BussinessApp
+//
+//  Created by Tiger Coder on 1/19/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
