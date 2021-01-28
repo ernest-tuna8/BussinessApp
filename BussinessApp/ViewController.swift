4be691729675272fac87d1202669bfb0669277b5
clc babischkin <n/a>
2021-01-28 08:02:22 -0600

    Complete

--- BussinessApp/ViewController.swift
+++ BussinessApp/ViewController.swift
@@ -8,10 +8,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var CravingsImageview: UIImageView!
+    @IBOutlet weak var hamburger: UIImageView!
+    @IBOutlet weak var fivestars: UIImageView!
+    @IBOutlet weak var shieldsymbol: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        CravingsImageview.image = UIImage(named: "cravings")
+        hamburger.image = UIImage(named: "burger")
+        fivestars.image = UIImage(named: "Stars")
+        shieldsymbol.image = UIImage(named: "shield")
     }
 
 
